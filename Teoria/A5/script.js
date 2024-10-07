695f6cb084d063bbdd6c8174d0802944fe0122c1
TubaBruce <102251162+TubaBruce@users.noreply.github.com>
2024-10-06 22:19:28 -0300

    Aula 5
    Adicionando aula5

---
+++ Teoria/A5/script.js
@@ -0,0 +1,25 @@
+//let entrada=prompt("Coloque o seu nome:");
+//window.alert(entrada)
+//console.log(entrada)
+/* for(let i=0;i <101;i++){
+    if (i%2!=0){
+        console.log(i);
+    }
+} */
+/* for(let i=0;i <501;i++){
+    if (i%5==0){
+        console.log(i);
+    }
+} */
+/* let entrada=prompt("Coloque um número positivo:");
+if(entrada>=0){
+    for(entrada;entrada>=0;entrada--){
+        console.log(entrada);
+    }
+}
+else{
+    window.alert("Número inválido")
+} */
+    let entrada=prompt("Coloque um número inteiro:");
+    for(entrada;entrada>0;entrada--){
+        console.log(entrada);
